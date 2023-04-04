82ca009a05f667ccae791fc3b2693b0bcf575b19
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-04 14:51:24 +0200

    stampa con ciclo foreach

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -2,19 +2,46 @@
 
 //inizializzo un array dei cibi preferiti 
 string[] cibi = {
-    "cotoletta",
     "pasta e fagioli",
+    "cotoletta",
+    "pasta e piselli",
     "pasta e patate",
     "parmigiana",
     "lasagna",
+    "salsiccia e cime di rapa",
+    "gateau",
+    "sushi",
+    "kebab",
 };
+
 
 // Stampa di quanti elementi ci sono in classifica
 Console.WriteLine($"All'interno della classifica sono presenti {cibi.Length} cibi!");
+Console.WriteLine("");
+
 
 // Stampa degli elementi della classifica dal primo all'ultimo
 //Esempio con il ciclo for
+Console.WriteLine("Stampa con ciclo for");
 for ( int i = 0; i < cibi.Length; i++ ) { 
     
     Console.WriteLine(cibi[i] );
 }
+Console.WriteLine("");
+
+// Esempio con ciclo foreach
+Console.WriteLine("Stampa con ciclo foreach");
+var n = 0;
+foreach ( var i in cibi ) {
+
+    Console.WriteLine($"{n + 1}: {i}");
+    n++;
+}
+Console.WriteLine("");
+
+
+//Stampa cibo primo in classifica
+Console.WriteLine($"Il cibo in primo posizione è : {cibi[0]}");
+
+//Stampa cibo ultimo in classifica
+Console.WriteLine($"Il cibo all`ultimo posizione  è : {cibi[9]}");
